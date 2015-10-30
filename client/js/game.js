61332d34cde75d6e3fdff1d5a66d733ae147f69f
Stephen Wu <stefan.wu@gmail.com>
2015-10-30 09:07:48 -0700

    Game movements working. Working on collision detection

--- client/js/game.js
+++ client/js/game.js
@@ -1,50 +1,91 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameContainer');
+window.currentDirection = null;
+window.GAME_WIDTH = 800;
+window.GAME_HEIGHT = 600;
+window.VELOCITY = 500;
+window.score = 0;
+var gameTimer = 0;
+
+var game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, 'gameContainer');
 
 var mainState = {
   preload: function () {
     game.stage.backgroundColor = '#666';
-    game.load.image('player', 'assets/hrlogo.png'); 
-    game.load.image('ground', 'assets/ground.png');
+    game.load.image('player', 'assets/player.png');
+    game.load.image('apple', 'assets/hrlogo.png');
+
   },
-  create: function () {
+  create: function () { // Initialize
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    this.player = this.game.add.sprite(100, 245, 'player');
+    this.player = this.game.add.sprite(100, 245, 'player'); // x, y, entity
     game.physics.arcade.enable(this.player);
-    this.player.body.gravity.y = 1000; 
-    this.player.body.bounce.y = 0.5;
+    this.player.body.velocity.y = VELOCITY;
     this.player.body.collideWorldBounds = true;
-    this.player.scale.setTo(0.4, 0.4);
+    this.apple = this.addApple();
 
-    this.platforms = game.add.group();
-    this.platforms.enableBody = true;
-    this.ground = [];
-    for (var i = 0; i < game.world.width; i+=70) {
-      this.ground.push(this.platforms.create(i, game.world.height - 70, 'ground'));
+  },
+  addApple: function() {
+    var apple = this.game.add.sprite(Math.floor(Math.random() * GAME_WIDTH - 25),
+      Math.floor(Math.random() * GAME_HEIGHT - 25), 'apple');
+
+    apple.scale.setTo(0.4, 0.4);
+    game.physics.arcade.enable(apple);
+    apple.body.immovable = true;
+
+    return apple;
+  },
+  deleteApple: function() {
+    this.apple.position.x = Math.floor(Math.random() * GAME_WIDTH - 25);
+    this.apple.position.y = Math.floor(Math.random() * GAME_HEIGHT - 25);
+  },
+  collision: function() {
+    // if player hits apple, deleteApple() and addApple()
+    if(Math.abs(this.player.position.x - this.apple.position.x) < 30 && Math.abs(this.player.position.y - this.apple.position.y) < 30) {
+      this.deleteApple();
+      score++;
     }
-    this.platforms.forEach(function(platform) {
-      platform.body.immovable = true;
-    });
-    
+    // this.deleteApple();
+    // console.log(score);
+    // // switch(currentDirection) {
+
+    // // }
+    // this.player.body.velocity.y = -VELOCITY;
   },
   update: function () {
-    for (var i = 0; i < this.ground.length; i++) {
-      game.physics.arcade.collide(this.player, this.ground[i]);
-    }
-    this.player.body.velocity.x = 0;
+    // Collision detection
+    // this.physics.arcade.collide(this.player, this.apple, this.collision, null, this);
 
+    this.collision();
+    // every time we touch an apple, generate a new apple
     var cursors = game.input.keyboard.createCursorKeys();
-    if (cursors.left.isDown)
+
+
+    if (cursors.left.isDown && currentDirection !== 'right')
     {
-        this.player.body.velocity.x = -300;
+        this.player.body.velocity.x = -VELOCITY;
+        this.player.body.velocity.y = 0;
+        currentDirection = 'left';
     }
-    else if (cursors.right.isDown)
+    else if (cursors.right.isDown && currentDirection !== 'left')
     {
-        this.player.body.velocity.x = 300;
+        this.player.body.velocity.x = VELOCITY;
+        this.player.body.velocity.y = 0;
+        currentDirection = 'right';
+
     }
-    if (cursors.up.isDown && this.player.body.touching.down)
+    if (cursors.up.isDown && currentDirection !== 'down')
     {
-        this.player.body.velocity.y = -800;
+        this.player.body.velocity.y = -VELOCITY;
+        this.player.body.velocity.x = 0;
+        currentDirection = 'up';
+
+    }
+    if (cursors.down.isDown && currentDirection !== 'up')
+    {
+        this.player.body.velocity.y = VELOCITY;
+        this.player.body.velocity.x = 0;
+        currentDirection = 'down';
+
     }
   }
 };
